c0a2e48aaeee87cf672ad8a15cfe5f3990c5ccb5
Muhammad Wahab Ansari <47778181+wahabansari@users.noreply.github.com>
2023-10-25 12:17:22 +0000

    complete conversion

--- javascript/03_conversionOperations.js
+++ javascript/03_conversionOperations.js
@@ -15,12 +15,12 @@ console.log(typeof (convertedvalue))
 /************ 
  * 
    ============= Booleans conversions =============
-   
+
  ************/
 
 let userIsLoggedIn = 1;
 
-console.log(Boolean(!userIsLoggedIn));
+// console.log(Boolean(!userIsLoggedIn));
 
 
 
@@ -30,7 +30,30 @@ console.log(Boolean(!userIsLoggedIn));
  
    ============= Operations =============
  
+   bonus point : Javascrpt always start calculation from left to right
 ************/
 
+// console.log("1" + 2)
+// console.log(1 + "2")
+
+
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// like we have something
+// console.log(+true); // not the standard way of code
+// console.log(true+); 
+
+
+/**** A quick question ****/
+// let num, num1, num2 // I have to assign one value to all of them
+// num = num1 = num2 = 3 + 5;
+
+let counter = 50
+
+let count = counter--;
+console.log(count);
+console.log(counter);
 
 
