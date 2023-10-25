83f4f9797d294d6861864d7e395cc72a5103d37a
Muhammad Wahab Ansari <47778181+wahabansari@users.noreply.github.com>
2023-10-25 11:47:24 +0000

    add the required files and commit the change of dataTypes and comnversions

---
+++ javascript/02_dataTypes.js
@@ -0,0 +1,11 @@
+/*  *************
+
+ Javascript in now days has many new features to use these features we have to implement "use strict" to use all the new feature of js
+
+ *************  */
+
+"use strict";
+
+console.log(typeof null)
+
+console.table([typeof null, typeof "", typeof {}])
---
+++ javascript/03_conversionOperations.js
@@ -0,0 +1,36 @@
+/************ 
+ * 
+    ============= Number Conversion =============
+   
+const num = "33123";
+console.log(typeof (num));
+
+let convertedvalue = Number(num);
+console.log(typeof (convertedvalue))
+ 
+************/
+
+
+
+/************ 
+ * 
+   ============= Booleans conversions =============
+   
+ ************/
+
+let userIsLoggedIn = 1;
+
+console.log(Boolean(!userIsLoggedIn));
+
+
+
+
+
+/************ 
+ 
+   ============= Operations =============
+ 
+************/
+
+
+
